0241c04f1e4795701fe03a77538ea9ff81019f68
Kartik Vijay Shanbhag <shanbhag.k@northeastern.edu>
2024-02-17 18:33:31 -0500

    Header and Footer Skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Landing from './Pages/Landing';
 
 function App() {
   return (
     <div className="App">
-
+           <Landing/>
     </div>
   );
 }
---
+++ frontend/src/Pages/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../Components/NavBar';
+import Footer from '../Components/Footer';
+import Carousel from '../Components/Carousel';
+
+function Landing() {
+    return (
+        <div>
+            <NavBar />
+            <Carousel /> 
+            <Footer />
+        </div>
+    )
+}
+
+export default Landing
