c65d9abbb15f504511345d018f9198a5f754c248
DeepanshChaturvedi <chaturvedi.de@northeastern.edu>
2024-02-17 17:12:29 -0500

    Added ingredients and user models

---
+++ backend/app/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+// import UserModel from "./User.js";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    // measurment: {
+    //     type: String,
+    //     required: true
+    // },
+},
+{
+    versionKey: false
+})
+
+const userSchema = mongoose.model('User', UserSchema);
+
+export default userSchema;
