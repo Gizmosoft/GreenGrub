030ee9b614bd1f50894eeb0b960032af7f7de986
DeepanshChaturvedi <chaturvedi.de@northeastern.edu>
2024-02-18 03:28:10 -0500

    Added const for different measurements

--- backend/app/services/recipe-service.js
+++ backend/app/services/recipe-service.js
@@ -2,6 +2,11 @@ import recipeModel from "../models/Recipe.js";
 import { CSVReader } from "../../middleware/csvParser.js";
 
 
+const TBL_SPOON = 14.8
+const T_SPOON = 5
+const CUP = 250
+const SIZE = 100
+
 // searchAll is a helper service function
 export const searchAll = async (params = {}) => {
     const recipes = recipeModel.find(params).exec(); // find() is a model function
@@ -10,7 +15,7 @@ export const searchAll = async (params = {}) => {
 
 // get Recipes by user Id
 export const getRecipesByUserId = async (owner) => {
-    const recipes = await recipeModel.find({owner}).exec();
+    const recipes = await recipeModel.find({ owner }).exec();
     return recipes;
 }
 
@@ -26,25 +31,28 @@ export const createRecipe = async (newRecipe) => {
     return recipe.save(); // save() is a model function
 };
 
-// create is a helper service function
+// Calculates emission and saves it in database
 export const calculateEmissions = async (newRecipe) => {
-    //const recipe = new recipeModel(newRecipe);
+    const recipe = new recipeModel(newRecipe);
     var calCount = 0;
     const records = await CSVReader();
-    //records.forEach(logMapElements);
 
     var ingredientsArray = newRecipe.ingredients;
 
     for (var i = 0; i < ingredientsArray.length; i++) {
         var name = ingredientsArray[i]["name"];
         var carbonCount = records.get(name);
-        calCount += ((ingredientsArray[i]["quantity"] * 15) / 1000) * carbonCount
+        var strComparer = ingredientsArray[i]["measurement"].toLowerCase().replace(/ +/g, "");
+        var quantity = 0;
+        switch (strComparer) {
+            case "teaspoon": quantity = T_SPOON;
+            case "tablespoon": quantity = TBL_SPOON;
+            case "cup": quantity = CUP;
+            case "": quantity = SIZE;
+            default: quantity = 100;
+        }
+        calCount += ((ingredientsArray[i]["quantity"] * quantity) / 1000) * carbonCount;
     }
-
-    //calCount += records.get("TUNA (F)");
-    return calCount;
+    recipe.emission = calCount;
+    return recipe.save();
 };
-
-function logMapElements(value, key, map) {
-    console.log(`m[${key}] = ${value}`);
-}
